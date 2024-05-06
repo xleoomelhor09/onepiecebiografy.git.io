5e717b52461a5d9a7f99d0bcb5e3764ffc67e8af
xleoomelhor09 <mercurioleonardo09@gmail.com>
2024-05-06 19:18:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,26 @@ function loadContent(page) {
             contentDiv.innerHTML = "<p>Erro ao carregar a página.</p>";
         });
 }
+
+function loadContent(page) {
+    const contentDiv = document.getElementById('content');
+    fetch(page)
+        .then(response => response.text())
+        .then(html => {
+            contentDiv.innerHTML = html;
+            const dynamicBio = contentDiv.querySelector('.bio-text');
+            if (dynamicBio) {
+                dynamicBio.style.display = 'block';
+            }
+        })
+        .catch(err => {
+            console.log("Erro ao carregar a página: ", err);
+            contentDiv.innerHTML = "<p>Erro ao carregar a página.</p>";
+        });
+}
+
+function showMainBio() {
+    const contentDiv = document.getElementById('content');
+    contentDiv.innerHTML = '';
+    document.getElementById('bio-one-piece').style.display = 'block';
+}
